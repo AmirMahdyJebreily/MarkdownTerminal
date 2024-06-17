3c56d0ea8b41974cca2e3e76d208f0eed40ef5ba
Amir JCS <amirmahdyjdx@gmail.com>
2024-06-17 21:45:24 +0330

    feat(components): add code components

--- src/mdcat/MDComponents.cs
+++ src/mdcat/MDComponents.cs
@@ -4,47 +4,62 @@ namespace MarkdownTerminal;
 
 public static class MDComponents
 {
-    public static string Heading(this string plainText, int grade)
+    private static Func<string, string> _mdSynTheme = plainText => plainText.Colorize(249).Decorate(UniTextDecoration.Dim);
+    public static string Heading(this string plainText, int grade, bool mdSyntax = true)
     {
-        string preRes = $"   {("").PadRight(grade, '#')
-                        .Colorize(249)
-                        .Decorate(UniTextDecoration.Dim)} ";
+        string mdSyn = (mdSyntax) ? $"   {_mdSynTheme(("").PadRight(grade, '#'))} " : "   ";
         string res = plainText.Decorate(UniTextDecoration.Bold);
         if ((grade != 1))
         {
-            return preRes + res.Colorize(257 - (grade * 2)); // Colorize the heading #{any but 1} text
+            return mdSyn + res.Colorize(257 - (grade * 2)); // Colorize the heading #{any but 1} text
         }
-        return preRes + res.Decorate(UniTextDecoration.Underline).Colorize(231); // Colorize the heading #1 text
+        return mdSyn + res.Decorate(UniTextDecoration.Underline).Colorize(231); // Colorize the heading #1 text
     }
-    public static string Paragraph(this string plainText)
+    public static string Paragraph(this string plainText, bool mdSyntax = true)
     {
-        return " - ".Colorize(237) + plainText + "\n";
+        return ((mdSyntax) ? " - ".Colorize(237) : "") + plainText + "\n";
     }
-    public static string MDBold(this string plainText)
+    public static string MDBold(this string plainText, bool mdSyntax = true)
     {
-        return plainText.Colorize(231).Decorate(UniTextDecoration.Bold); // white
-    }
-
-    public static string MDItalic(this string plainText)
-    {
-        return plainText.Colorize(231).Decorate(UniTextDecoration.Italic); // white
+        string mdSyn = (mdSyntax) ? _mdSynTheme("**") : "";
+        return mdSyn + plainText.Decorate(UniTextDecoration.Bold) + mdSyn; // white
     }
 
-    public static string MDUnderline(this string plainText)
+    public static string MDItalic(this string plainText, bool mdSyntax = true)
     {
-        return plainText.Decorate(UniTextDecoration.Underline); // white
+        string mdSyn = (mdSyntax) ? _mdSynTheme("*") : "";
+        return mdSyn + plainText.Decorate(UniTextDecoration.Italic) + mdSyn; // white
     }
 
-    public static string MDStrikethrough(this string plainText)
+    public static string MDUnderline(this string plainText, bool mdSyntax = true)
     {
-        return plainText.Decorate(UniTextDecoration.Strikethrough); // white
+        string mdSyn = (mdSyntax) ? _mdSynTheme("_") : "";
+        return mdSyn + plainText.Decorate(UniTextDecoration.Underline) + mdSyn; // white
     }
 
-    public static string MDQoute(this string plainText)
+    public static string MDStrikethrough(this string plainText, bool mdSyntax = true)
     {
-        const int colorCode = 237;
-        return $"█    {">".Colorize(240)} {plainText} ".Colorize(colorCode, UniColorGround.Background);
+        string mdSyn = (mdSyntax) ? _mdSynTheme("~~") : "";
+        return mdSyn + plainText.Decorate(UniTextDecoration.Strikethrough) + mdSyn; // white
+    }
 
+    public static string MDQoute(this string plainText, bool mdSyntax = true)
+    {
+        string mdSyn = (mdSyntax) ? _mdSynTheme(">") : "";
+        return $"█    {mdSyn} {plainText} ".Colorize(237, UniColorGround.Background);
+    }
+
+    public static string MdInlineCode(this string plainText, bool mdSyntax = true)
+    {
+        string mdSyn = (mdSyntax) ? _mdSynTheme("`") : "";
+        return mdSyn + plainText.Colorize(210) + mdSyn;
+    }
+
+    public static string MdCodeBlock(this string plainText, string lang = "", bool mdSyntax = true)
+    {
+        string mdSyn = (mdSyntax) ? _mdSynTheme("```") : "";
+        string mdSynSec = (mdSyntax) ? _mdSynTheme(lang) : "";
+        return $"\n   {mdSyn}{mdSynSec}\n\t{plainText}\n   {mdSyn}\n";
     }
 
 }
--- src/mdcat/Program.cs
+++ src/mdcat/Program.cs
@@ -9,11 +9,12 @@ public class mdcat
     {
         try
         {
-            string h1 = MDComponents.Heading("Kos " + "Madar".MDItalic() + " Besat", 1);
+            string h1 = MDComponents.Heading("Kos Madar Besat", 1);
             Console.WriteLine(h1);
             Console.WriteLine(MDComponents.Paragraph($"Madar besat was a {"whore".MDBold()} in mashhad, stari st no.13"));
             Console.WriteLine(MDComponents.Heading("Kos Madar Besat", 2));
-            Console.WriteLine(MDComponents.MDQoute($"Madar besat was a {"whore".MDItalic()} in mashhad, stari st no.13"));
+            Console.WriteLine(MDComponents.MDQoute($"Madar besat was a {"whore".MdInlineCode()} in mashhad, stari st no.13"));
+            Console.WriteLine("Console.Writeline(\"Hello, world\")".MdCodeBlock("csharp"));
         }
         catch (MdcatIOException e)
         {
